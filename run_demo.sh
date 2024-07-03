dad6644633a2c3f5be4c70444d1399e59be26a61
Michael Booth <michael@databooth.com.au>
2024-07-03 15:02:15 +1000

    Tutorial final commits

--- run_demo.sh
+++ run_demo.sh
@@ -20,7 +20,34 @@ sdf compile
 
 sdf compile --show all
 
-# End of first tutorial
+# End of first tutorial / 2nd tutorial
+
+# Created new sql moms_flower_shop/models/analytics/dim_marketing_campaigns.sql
+
+# Validate the Model Through Static Analysis
+
+sdf compile
+
+sdf run analytics.dim_marketing_campaigns
+
+# Debugging
+
+# Understand Column Lineage
+
+sdf lineage analytics.dim_marketing_campaigns --column total_num_installs
+
+# Compare similar data
+
+sdf lineage staging.customers --column campaign_id
+
+# Make fix _v2
+
+sdf compile 
+
+sdf lineage staging.stg_installs_per_campaign --forward
+
+sdf run analytics.dim_marketing_campaigns
+
 
 
 
