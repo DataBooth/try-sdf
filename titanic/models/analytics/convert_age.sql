9da14ae02c61a84a313273ec86276b05beb33932
Michael Booth <michael@databooth.com.au>
2024-07-04 13:08:36 +1000

    Progress

--- titanic/models/analytics/convert_age.sql
+++ titanic/models/analytics/convert_age.sql
@@ -1,11 +1,11 @@
-WITH titanic_drop AS
+WITH titanic_convert_age AS
 (
 	SELECT  *
-	FROM staging.rename_cols
+	FROM staging.drop_cols
 )
 SELECT  passenger_class
        ,survived_indicator
        ,full_name
        ,sex
        ,age
-FROM titanic.staging.rename_cols
+FROM titanic.staging.drop_cols
