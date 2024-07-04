b87d146f164c212b8e851be3cfdda8f39d4f5fdd
Michael Booth <michael@databooth.com.au>
2024-07-04 13:49:51 +1000

    Add another conversion

--- titanic/models/analytics/convert_age.sql
+++ titanic/models/analytics/convert_age.sql
@@ -1,9 +1,13 @@
 WITH titanic_convert_age AS
 (
 	SELECT  *
+
 	FROM staging.drop_cols
 )
 SELECT  passenger_class
+       ,CASE WHEN survived_indicator = '1' OR survived_indicator = 1 THEN TRUE
+             WHEN survived_indicator = '0' OR survived_indicator = 0 THEN FALSE  ELSE NULL -- for any other values that are not 0 or 1
+             END          AS survived_bool
        ,survived_indicator
        ,full_name
        ,sex
