bff244246e7bd2935cb6a13c84ef20ca206d8c2f
Michael Booth <michael@databooth.com.au>
2024-07-04 11:20:26 +1000

    Progress update

--- titanic/models/staging/drop_cols.sql
+++ titanic/models/staging/drop_cols.sql
@@ -1,24 +1,11 @@
-/*
- ## PURPOSE: Discard redundant columns from the analysis
- 
- ## DISCARDED COLUMNS:
- sibsp           Number of Siblings/Spouses Aboard
- parch           Number of Parents/Children Aboard
- ticket          Ticket Number
- fare            Passenger Fare
- cabin           Cabin
- embarked        Port of Embarkation: (C = Cherbourg; Q = Queenstown; S = Southampton)
- boat            Lifeboat
- body            Body Identification Number
- home.dest       Home/Destination
- */
-WITH titanic_drop AS (
-  SELECT *
-  FROM staging.raw_titanic
+WITH titanic_drop AS
+(
+	SELECT  *
+	FROM staging.rename
 )
-SELECT passenger_class,
-  survived_indicator,
-  full_name,
-  sex,
-  age
-FROM titanic.staging.raw_titanic
+SELECT  passenger_class
+       ,survived_indicator
+       ,full_name
+       ,sex
+       ,age
+FROM titanic.staging.rename
---
+++ titanic/models/staging/rename.sql
@@ -0,0 +1,24 @@
+WITH source AS
+(
+	SELECT  *
+	FROM raw.raw_titanic
+), renamed AS
+(
+	SELECT  pclass    AS passenger_class
+	       ,survived  AS survived_indicator
+	       ,name      AS full_name
+	       ,sex
+	       ,age
+	       ,sibsp     AS n_siblings_spouse
+	       ,parch     AS n_parent_child
+	       ,ticket    AS ticket_number
+	       ,fare
+	       ,cabin
+	       ,embarked  AS port_embarked
+	       ,boat      AS life_boat
+	       ,body      AS body_id
+	       ,home_dest AS home_or_dest
+	FROM source
+)
+SELECT  *
+FROM renamed;
